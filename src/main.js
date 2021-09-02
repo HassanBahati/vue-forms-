48fd56f70a4503ee66555aa2ca2aa32ab40a3b08
HassanBahati <mukisabahati@gmail.com>
2021-09-02 18:40:27 +0300

    initial commit

--- src/main.js
+++ src/main.js
@@ -1,12 +1,12 @@
-import Vue from "vue";
-import App from "./App.vue";
-import router from "./router";
-import vuetify from "./plugins/vuetify";
+import Vue from 'vue'
+import App from './App.vue'
+import router from './router'
+import vuetify from './plugins/vuetify'
 
-// createApp(App).use(router).mount("#app");
+Vue.config.productionTip = false
 
 new Vue({
   router,
   vuetify,
-  render: (h) => h(App),
-}).$mount("#app");
+  render: h => h(App)
+}).$mount('#app')
